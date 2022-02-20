1a5eed97b48eaffd7fa5bea15fce2f4a5a4a8e0e
crepantherx <crepantherx@gmail.com>
2022-02-20 20:27:20 +0530

    v2022.02.20.b

--- question/views.py
+++ question/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+users = [
+    {
+        'name': 'sudhir',
+        'course': 'sql'
+    },
+    {
+        'name': 'utkarsh',
+        'course': 'python'
+    }
+]
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'users': users}
+    return render(request, 'question/index.html', context)
+
